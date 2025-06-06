695bb90ae903a69b0ae8665106381fc3b76f085b
jsndz <jaisondz9360@gmail.com>
2025-06-06 16:14:23 +0530

    files

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,21 @@ import { Probot } from "probot";
 
 export default (app: Probot) => {
   app.on("push", async (context) => {
-    console.log(`Received a push event for ${context.payload}`);
-  });
+    const changedFiles = context.payload.commits.flatMap((commit) => [
+      ...commit.added,
+      ...commit.modified,
+    ]);
+    for (var i = 0; i < changedFiles.length; i++) {
+      const { data } = await context.octokit.repos.getContent({
+        owner: context.payload.repository.owner.login,
+        repo: context.payload.repository.name,
+        path: changedFiles[i],
+        ref: context.payload.after,
+      });
 
+      if (!("content" in data)) continue;
+      const content = Buffer.from(data.content, "base64").toString("utf-8");
+      console.log(content);
+    }
+  });
 };
