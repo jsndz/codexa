86b0bec5103cf89e23c0a2d059b6b746542f1391
jsndz <jaisondz9360@gmail.com>
2025-06-24 14:14:50 +0530

    server: CORS

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,10 +1,17 @@
 import express from "express";
 import bodyParser from "body-parser";
+import cors from "cors";
 import { PORT } from "./config/config";
 
 const app = express();
 
 app.use(bodyParser.json());
+const corsOptions = {
+  origin: "http://localhost:5173",
+  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+  allowedMethods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+};
+app.use(cors(corsOptions));
 
 app.post("/", (req, res) => {
   console.log(req.body);
