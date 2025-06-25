8873dd94939c3d869f0a454218265b6512a4a696
jsndz <jaisondz9360@gmail.com>
2025-06-25 19:27:05 +0530

    auth

--- server/src/config/config.ts
+++ server/src/config/config.ts
@@ -1,15 +1,17 @@
 import dotenv from "dotenv";
-import fs from "fs";
+import { readFileSync } from "fs";
+import { fileURLToPath } from "url";
 import path from "path";
 
 dotenv.config();
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 export const PORT = process.env.PORT;
 export const APP_ID = process.env.APP_ID;
-export const PRIVATE_KEY = fs
-  .readFileSync(
-    path.resolve(__dirname, "../../" + process.env.PRIVATE_KEY_PATH!)
-  )
-  .toString();
+export const PRIVATE_KEY = readFileSync(
+  path.resolve(__dirname, "../../" + process.env.PRIVATE_KEY_PATH)
+).toString();
 
 export const WEBHOOK_SECRET = process.env.WEBHOOK_SECRET;
--- server/src/index.ts
+++ server/src/index.ts
@@ -4,15 +4,13 @@ import cors from "cors";
 import { Octokit } from "@octokit/rest";
 import { createAppAuth } from "@octokit/auth-app";
 
-import { PORT, APP_ID, PRIVATE_KEY } from "./config/config";
+import { PORT, APP_ID, PRIVATE_KEY } from "./config/config.js";
 
 const app = express();
 const auth = createAppAuth({
   appId: APP_ID!,
   privateKey: PRIVATE_KEY,
 });
-
-const appAuthentication = await auth({ type: "app" });
 
 app.use(bodyParser.json());
 const corsOptions = {
@@ -25,6 +23,9 @@ app.use(cors(corsOptions));
 app.post("/", async (req, res) => {
   try {
     const { iid } = req.body;
+    if (!iid) {
+      res.status(400).json({ message: "Installation ID is required" });
+    }
 
     const installationAuth = await auth({
       type: "installation",
